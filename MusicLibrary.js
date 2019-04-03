8afb4fdb43cafecd6d52ac7042e1a8325360c0e8
ferrazf <ferrazf9@gmail.com>
2019-04-03 21:41:51 +0000

    Music Library With Methods Assignment

---
+++ MusicLibrary.js
@@ -0,0 +1,29 @@
+var library = {
+    tracks: { 123: { id: 123,
+                     name: "Code Monkey",
+                     artist: "Jonathan Coulton",
+                     album: "Thing a Week Three" },
+              456: { id: 456,
+                     name: "Model View Controller",
+                     artist: "James Dempsey",
+                     album: "WWDC 2003"}
+            },
+  
+    playlists: { 789: { id: 789,
+                        name: "Coding Music",
+                        tracks: [123, 456]
+                      }
+               }
+  };
+
+  function printPlaylists(lib)
+  {
+    for(var plList in lib.playlists)
+    {
+        console.log(plList.id);
+        console.log(plList.name);
+        console.log(plList.tracks);
+    }
+  }
+
+  printPlaylists(library);
